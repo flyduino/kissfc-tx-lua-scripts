353c5ca9ff17973d655b27b005905aec029cfed2
Alexander Fedorov <fedor@anuta.org>
2017-03-05 10:01:36 +0100

    Some realignment

--- KissX7.lua
+++ KissX7.lua
@@ -412,25 +412,24 @@ SetupPages = {
       title = "PIDs",
       text = {
          { t = "P", x = 45, y = 14 },
-         { t = "I", x = 75, y = 14 },
-         { t = "D", x = 105, y = 14 },
+         { t = "I", x = 78, y = 14 },
+         { t = "D", x = 107, y = 14 },
          { t = "Roll",  x = 3,  y = 25 },
          { t = "Pitch", x = 3,  y = 38 },
          { t = "Yaw",   x = 3,  y = 50 }
       },
       fields = {
-         -- P
          { x = 20,  y = 25, i=1, max=65000, prec=2 },
-         { x = 47,  y = 25, i=2, max=65000, prec=3 },
-         { x = 75, y = 25, i=3, max=65000, prec=2 },
-         -- I
+         { x = 48,  y = 25, i=2, max=65000, prec=3 },
+         { x = 80,  y = 25, i=3, max=65000, prec=2 },
+         
          { x = 20,  y = 38, i=4, max=65000, prec=2 },
-         { x = 47, y = 38, i=5, max=65000, prec=3 },
-         { x = 75, y = 38, i=6, max=65000, prec=2 },
-         -- D
+         { x = 48,  y = 38, i=5, max=65000, prec=3 },
+         { x = 80,  y = 38, i=6, max=65000, prec=2 },
+         
          { x = 20,  y = 50, i=7, max=65000, prec=2 },
-         { x = 47,  y = 50, i=8, max=65000, prec=3 },
-         { x = 75, y = 50, i=9, max=65000, prec=2 }
+         { x = 48,  y = 50, i=8, max=65000, prec=3 },
+         { x = 80,  y = 50, i=9, max=65000, prec=2 }
       },
       read  = KISS_GET_PIDS,
       write = KISS_SET_PIDS,
@@ -440,26 +439,26 @@ SetupPages = {
    {
       title = "Rates",
       text = {
-         { t = "RC Rt", 	x = 31,  y = 14 },
-         { t = "Rate",    x = 65, y = 14 },
-         { t = "RC Crv", 	x = 95, y = 14 },
-         { t = "Roll",  x = 3,  y = 25 },
-         { t = "Pitch", x = 3,  y = 38 },
-         { t = "Yaw",   x = 3,  y = 50 }
+         { t = "RC",     x = 41, y = 14 },
+         { t = "Rate",   x = 65, y = 14 },
+         { t = "Curve",  x = 95, y = 14 },
+         { t = "Roll",   x = 3,  y = 25 },
+         { t = "Pitch",  x = 3,  y = 38 },
+         { t = "Yaw",    x = 3,  y = 50 }
       },
       fields = {
          -- RC Rate
          { x = 16,  y = 25, i=1, max=65000, prec=2 },
          { x = 48,  y = 25, i=2, max=65000, prec=2 },
-         { x = 80, y = 25, i=3, max=65000, prec=2 },
+         { x = 80,  y = 25, i=3, max=65000, prec=2 },
          -- Rate
          { x = 16,  y = 38, i=4, max=65000, prec=2 },
          { x = 48,  y = 38, i=5, max=65000, prec=2 },
-         { x = 80, y = 38, i=6, max=65000, prec=2 },
+         { x = 80,  y = 38, i=6, max=65000, prec=2 },
          -- RC Curve
          { x = 16,  y = 50, i=7, max=65000, prec=2 },
          { x = 48,  y = 50, i=8, max=65000, prec=2 },
-         { x = 80, y = 50, i=9, max=65000, prec=2 }
+         { x = 80,  y = 50, i=9, max=65000, prec=2 }
       },
       read  = KISS_GET_RATES,
       write = KISS_SET_RATES,
@@ -469,28 +468,27 @@ SetupPages = {
    {
       title = "Filters",
       text = {
-        -- { t = "Notch",  x = 47,  y = 14 },
-         { t = "Ctr", 				x = 65, y = 14 },
+         { t = "Center", 			x = 50, y = 14 },
          { t = "Cutoff", 			x = 90, y = 14 },
-         { t = "Notch Fltr", 	x = 3,  y = 14 },
+         { t = "Notch", 			x = 3,  y = 14 },
          { t = "Roll",  			x = 3,  y = 25 },
          { t = "Pitch", 			x = 3,  y = 38 },
-         { t = "LPF",   			x = 3,  y = 50 },
-         { t = "Yaw",   			x = 82, y = 50 }
+         { t = "LPF",   			x = 3,  y = 52 },
+         { t = "Yaw",   			x = 65, y = 52 }
       },
       lines = {
-      	 { x1 = 4, y1 = 48, x2 = 190, y2 = 48 }
+      	 { x1 = 1, y1 = 48, x2 = 125, y2 = 48 }
       },
       fields = {
          -- Filters
          { x = 15,  y = 25,  i=1, min=1, max=2, table = { "Off", "On" }},
          { x = 45,  y = 25,  i=2, min=0, max=490 },
-         { x = 80, y = 25,  i=3, min=0, max=490 },
+         { x = 80,  y = 25,  i=3, min=0, max=490 },
          { x = 15,  y = 38,  i=4, min=1, max=2, table = { "Off", "On" }},
          { x = 45,  y = 38,  i=5, min=0, max=490 },
-         { x = 80, y = 38,  i=6, min=0, max=490 },
-         { x = 15,  y = 50,  i=7, min=1, max=7, table = { "Off", "High", "Med. High", "Medium", "Med. Low", "Low", "Very Low" } },
-         { x = 82, y = 50,  i=8, min=0, max=97},
+         { x = 80,  y = 38,  i=6, min=0, max=490 },
+         { x = 15,  y = 52,  i=7, min=1, max=7, table = { "Off", "High", "MedHi", "Med", "MedLo", "Low", "VerLo" } },
+         { x = 80,  y = 52,  i=8, min=0, max=97},
       },
       read  = KISS_GET_FILTERS,
       write = KISS_SET_FILTERS,
@@ -502,8 +500,8 @@ SetupPages = {
       text = {},
       fields = {
          -- Alarms
-         { t = "VBat",    x = 15,  y = 25, sp = 30, i=1, min=0, max=26000, prec=1 },
-         { t = "mAH",     x = 15, y = 45, sp = 30, i=2, min=0, max=26000, inc=10 }
+         { t = "VBat",    x = 15,  y = 27, sp = 70, i=1, min=0, max=26000, prec=1 },
+         { t = "mAH",     x = 15,  y = 40, sp = 70, i=2, min=0, max=26000, inc=10 }
       },
       read  = KISS_GET_ALARMS,
       write = KISS_SET_ALARMS,
@@ -516,9 +514,9 @@ SetupPages = {
       fields = {
          -- VTX
          { t = "Band",    	   x = 15,  y = 14, sp = 70, i=2, min=1, max=5, table = { "A", "B", "E", "FS", "RB" } },
-         { t = "Channel",      x = 15, y = 27, sp = 70, i=3, min=1, max=8 },
+         { t = "Channel",      x = 15,  y = 27, sp = 70, i=3, min=1, max=8 },
          { t = "Low Power",    x = 15,  y = 40, sp = 70, i=4, min=0, max=600 },
-         { t = "High Power",   x = 15, y = 53, sp = 70, i=5, min=0, max=600 }
+         { t = "High Power",   x = 15,  y = 53, sp = 70, i=5, min=0, max=600 }
       },
       read  = KISS_GET_VTX_CONFIG,
       write = KISS_SET_VTX_CONFIG,
