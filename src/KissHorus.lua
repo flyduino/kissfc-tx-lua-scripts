065ac22c2bfbcab4b0a6f93f90734d5a2e0eebae
Jan-Martijn Wijnholds <jm.wijnholds@gmail.com>
2017-04-10 21:14:54 +0200

    Fixed the layout for PID, Rates and Notch Filter pages

--- src/KissHorus.lua
+++ src/KissHorus.lua
@@ -4,12 +4,12 @@ SetupPages = {
    {
       title = "PIDs",
       text = {
-         { t = "P",      x = 149,  y =  68 },
+         { t = "P",      x = 151,  y =  68 },
          { t = "I",      x = 229,  y =  68 },
          { t = "D",      x = 309,  y =  68 },
-         { t = "ROLL",   x =  35,  y =  96 },
-         { t = "PITCH",  x =  35,  y = 124 },
-         { t = "YAW",    x =  35,  y = 152 }
+         { t = "Roll",   x =  35,  y =  96 },
+         { t = "Pitch",  x =  35,  y = 124 },
+         { t = "Yaw",    x =  35,  y = 152 }
       },
       fields = {
          -- P
@@ -33,26 +33,26 @@ SetupPages = {
    {
       title = "Rates",
       text = {
-         { t = "RC Rate", x = 129,  y = 68 },
-         { t = "Rate",    x = 209, y = 68 },
+         { t = "RC Rate",  x = 139, y = 68 },
+         { t = "Rate",     x = 227, y = 68 },
          { t = "RC Curve", x = 289, y = 68 },
-         { t = "Roll",  x = 35,  y = 96 },
-         { t = "Pitch", x = 35,  y = 124 },
-         { t = "Yaw",   x = 35,  y = 152 }
+         { t = "Roll",     x = 35,  y = 96 },
+         { t = "Pitch",    x = 35,  y = 124 },
+         { t = "Yaw",      x = 35,  y = 152 }
       },
       fields = {
          -- RC Rate
          { x = 129,  y = 96, i=1, max=65000, prec=2 },
          { x = 209,  y = 96, i=2, max=65000, prec=2 },
-         { x = 289, y = 96, i=3, max=65000, prec=2 },
+         { x = 289,  y = 96, i=3, max=65000, prec=2 },
          -- Rate
          { x = 129,  y = 124, i=4, max=65000, prec=2 },
          { x = 209,  y = 124, i=5, max=65000, prec=2 },
-         { x = 289, y = 124, i=6, max=65000, prec=2 },
+         { x = 289,  y = 124, i=6, max=65000, prec=2 },
          -- RC Curve
          { x = 129,  y = 152, i=7, max=65000, prec=2 },
          { x = 209,  y = 152, i=8, max=65000, prec=2 },
-         { x = 289, y = 152, i=9, max=65000, prec=2 }
+         { x = 289,  y = 152, i=9, max=65000, prec=2 }
       },
       read  = KISS_GET_RATES,
       write = KISS_SET_RATES,
@@ -62,28 +62,27 @@ SetupPages = {
    {
       title = "Filters",
       text = {
-        -- { t = "Notch",  x = 47,  y = 14 },
-         { t = "Center", x = 180, y = 68 },
-         { t = "Cutoff", x = 280, y = 68 },
-         { t = "Notch Filter", x = 70,  y = 68 },
-         { t = "Roll",  x = 35,  y = 96 },
-         { t = "Pitch", x = 35,  y = 124 },
-         { t = "LPF",   x = 35,  y = 158 },
-         { t = "Yaw",   x = 35, y = 186 }
+         { t = "Notch Filter", x = 113, y = 68 },
+         { t = "Center",       x = 220, y = 68 },
+         { t = "Cutoff",       x = 300, y = 68 },
+         { t = "Roll",         x = 35,  y = 96 },
+         { t = "Pitch",        x = 35,  y = 124 },
+         { t = "LPF",          x = 35,  y = 158 },
+         { t = "Yaw",          x = 35,  y = 186 }
       },
       lines = {
       	 { x1 = 4, y1 = 48, x2 = 190, y2 = 48 }
       },
       fields = {
          -- Filters
-         { x = 100,  y = 96,  i=1, min=1, max=2, table = { "Off", "On" }},
-         { x = 180,  y = 96,  i=2, min=0, max=490 },
-         { x = 280, y = 96,  i=3, min=0, max=490 },
-         { x = 100,  y = 124,  i=4, min=1, max=2, table = { "Off", "On" }},
-         { x = 180,  y = 124,  i=5, min=0, max=490 },
-         { x = 280, y = 124,  i=6, min=0, max=490 },
-         { x = 100,  y = 158,  i=7, min=1, max=7, table = { "Off", "High", "Med. High", "Medium", "Med. Low", "Low", "Very Low" } },
-         { x = 100, y = 186,  i=8, min=0, max=97},
+         { x = 129,  y = 96,   i=1, min=1, max=2, table = { "Off", "On" }},
+         { x = 209,  y = 96,   i=2, min=0, max=490 },
+         { x = 289,  y = 96,   i=3, min=0, max=490 },
+         { x = 129,  y = 124,  i=4, min=1, max=2, table = { "Off", "On" }},
+         { x = 209,  y = 124,  i=5, min=0, max=490 },
+         { x = 289,  y = 124,  i=6, min=0, max=490 },
+         { x = 129,  y = 158,  i=7, min=1, max=7, table = { "Off", "High", "Med. High", "Medium", "Med. Low", "Low", "Very Low" } },
+         { x = 129,  y = 186,  i=8, min=0, max=97},
       },
       read  = KISS_GET_FILTERS,
       write = KISS_SET_FILTERS,
@@ -96,7 +95,7 @@ SetupPages = {
       fields = {
          -- Alarms
          { t = "VBat",    x = 100,  y = 96, sp = 60, i=1, min=0, max=26000, prec=1 },
-         { t = "mAH",     x = 250, y = 96, sp = 60, i=2, min=0, max=26000, inc=10 }
+         { t = "mAH",     x = 250,  y = 96, sp = 60, i=2, min=0, max=26000, inc=10 }
       },
       read  = KISS_GET_ALARMS,
       write = KISS_SET_ALARMS,
@@ -108,8 +107,8 @@ SetupPages = {
       text = {},
       fields = {
          -- VTX
-         { t = "Band",    	   x = 35,  y = 96, sp = 100, i=2, min=1, max=5, table = { "A", "B", "E", "FS", "RB" } },
-         { t = "Channel",      x = 210, y = 96, sp = 120, i=3, min=1, max=8 },
+         { t = "Band",    	   x = 35,  y = 96,  sp = 100, i=2, min=1, max=5, table = { "A", "B", "E", "FS", "RB" } },
+         { t = "Channel",      x = 210, y = 96,  sp = 120, i=3, min=1, max=8 },
          { t = "Low Power",    x = 35,  y = 158, sp = 100, i=4, min=0, max=600 },
          { t = "High Power",   x = 210, y = 158, sp = 120, i=5, min=0, max=600 }
       },
