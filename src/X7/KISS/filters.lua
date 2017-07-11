f680654512edf5e1cf3e08156e5e441534a067fb
awolf78 <awolf78@gmx.de>
2017-07-11 02:49:54 -0400

    Added new LPF for X7 and Horus (#20)

--- src/X7/KISS/filters.lua
+++ src/X7/KISS/filters.lua
@@ -12,6 +12,8 @@ return {
     ret[6] = bit32.lshift(values[11], 8) + values[12]
     ret[7] = values[1] + 1
     ret[8] = values[2]
+    ret[9] = values[13] + 1
+    ret[10] = values[14] + 1
     return ret
   end,
 
@@ -29,30 +31,36 @@ return {
     ret[10] = bit32.band(values[5], 0xFF)
     ret[11] = bit32.band(bit32.rshift(values[6], 8), 0xFF)
     ret[12] = bit32.band(values[6], 0xFF)
+    ret[13] = bit32.band(values[9]-1, 0xFF)
+    ret[14] = bit32.band(values[10]-1, 0xFF)
     return ret
   end,
   title = "Filters",
   text = {
-    { t = "Center",      x = 50, y = 14 },
-    { t = "Cutoff",      x = 90, y = 14 },
-    { t = "Notch",       x = 3,  y = 14 },
-    { t = "Roll",        x = 3,  y = 25 },
-    { t = "Pitch",       x = 3,  y = 38 },
-    { t = "LPF",         x = 3,  y = 52 },
-    { t = "Yaw",         x = 65, y = 52 }
+    { t = "Center",      x = 50, y = 10 },
+    { t = "Cutoff",      x = 90, y = 10 },
+    { t = "Notch",       x = 3,  y = 10 },
+    { t = "Roll",        x = 3,  y = 20 },
+    { t = "Pitch",       x = 3,  y = 30 },
+    { t = "R/P",         x = 3,  y = 42 },
+    { t = "Yaw f.",      x = 65, y = 42 },
+    { t = "Yaw",         x = 3,  y = 52 },
+    { t = "Dterm",       x = 65, y = 52 }
   },
   lines = {
-    { x1 = 1, y1 = 48, x2 = 125, y2 = 48 }
+    { x1 = 1, y1 = 39, x2 = 125, y2 = 39 }
   },
   fields = {
     -- Filters
-    { x = 15,  y = 25,  i=1, min=1, max=2, table = { "Off", "On" }},
-    { x = 45,  y = 25,  i=2, min=0, max=490 },
-    { x = 80,  y = 25,  i=3, min=0, max=490 },
-    { x = 15,  y = 38,  i=4, min=1, max=2, table = { "Off", "On" }},
-    { x = 45,  y = 38,  i=5, min=0, max=490 },
-    { x = 80,  y = 38,  i=6, min=0, max=490 },
-    { x = 15,  y = 52,  i=7, min=1, max=7, table = { "Off", "High", "MedHi", "Med", "MedLo", "Low", "VerLo" } },
-    { x = 80,  y = 52,  i=8, min=0, max=97},
+    { x = 15,  y = 20,  i=1,  min=1, max=2, table = { "Off", "On" }},
+    { x = 45,  y = 20,  i=2,  min=0, max=490 },
+    { x = 80,  y = 20,  i=3,  min=0, max=490 },
+    { x = 15,  y = 30,  i=4,  min=1, max=2, table = { "Off", "On" }},
+    { x = 45,  y = 30,  i=5,  min=0, max=490 },
+    { x = 80,  y = 30,  i=6,  min=0, max=490 },
+    { x = 15,  y = 42,  i=7,  min=1, max=7, table = { "Off", "High", "MedHi", "Med", "MedLo", "Low", "VerLo" } },
+    { x = 80,  y = 42,  i=8,  min=0, max=97},
+    { x = 15,  y = 52,  i=9,  min=1, max=7, table = { "Off", "High", "MedHi", "Med", "MedLo", "Low", "VerLo" } },
+    { x = 80,  y = 52,  i=10, min=1, max=7, table = { "Off", "High", "MedHi", "Med", "MedLo", "Low", "VerLo" } },
   }
 }
